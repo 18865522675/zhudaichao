3c0c50a1bc8ad01a2f3e55e59e8937ba655cfdff
zbj <178403135@qq.com>
2019-03-19 11:13:25 +0800

    2222

--- src/views/productManage/creditCard.vue
+++ src/views/productManage/creditCard.vue
@@ -343,7 +343,7 @@
 				</el-form>
 			  </div>
 			  <span slot="footer" class="dialog-footer">
-			    <el-button @click="dialogVisible = false">取 消</el-button>
+			    <el-button @click="addBankDialogVisible = false">取 消</el-button>
 			    <el-button type="primary" @click="sureAddBank('bankForm')">确 定</el-button>
 			  </span>
 			</el-dialog>
